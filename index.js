ccd8f7f3ff431f67ee997a2142e86b901f408693
wenjie.xu <wenjie.xu@bianlifeng.com>
2019-05-17 11:36:14 +0800

    解决MIME报错

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const Koa = require('koa');
 const app = new Koa();
+const koaStatic = require('koa-static');
+
 const fs = require('fs');
 const exec = require('child_process').exec;
 
+// 第一种方式：使用koa-static中间件
+// app.use(koaStatic(__dirname))
+
+
+// 第二种方式：设置响应
 app.use(ctx => {
-  // console.log(ctx.url)
-  // const typeMap = {
-  //   'js': 'text/javascript',
-  //   '/': 'text/html',
-  // }
-  // console.log(typeMap[ctx.url.split('.').pop()])
-  // ctx.type = typeMap[ctx.url.split('.').pop()];
-  ctx.body = fs.readFileSync('./index.html', 'utf8');
+  const typeMap = {
+    'js': 'text/javascript',
+    '/': 'text/html',
+  }
+  ctx.type = typeMap[ctx.url.split('.').pop()];
+  if (ctx.url === '/') ctx.url = '/index.html';
+  ctx.body = fs.readFileSync(__dirname + ctx.url, 'utf8');
 })
 
 
@@ -19,5 +25,5 @@ app.listen(4000)
 
 console.log('listening port 4000')
 
-exec(" open http://localhost:4000")
+// exec(" open http://localhost:4000")
 
