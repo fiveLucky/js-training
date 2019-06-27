37bd3758faffc86cf6ca168a7fafa27c3ff1b5e9
wenjie.xu <wenjie.xu@bianlifeng.com>
2019-06-27 10:59:32 +0800

    增加new实现

--- Object/new.js
+++ Object/new.js
@@ -1,14 +1,25 @@
 /**
  * 模拟实现 new 操作符
  * 分析：new 操作符做了什么？
- *  1、目标函数被执行
- *  2、生成一个新对象
- *  3、新对象绑定this
- *  4、执行【Proto】
+ *  1、生成一个新对象
+ *  2、执行原型链接
+ *  3、目标函数被执行，绑定this
+ *  4、新对象绑定this
+ *  5、返回新对象/构造函数返回的对象
  *  
  */
 
-function myNew(target) {
-
+function myNew(func, ...arg) {
+  // 创建对象
+  var target = {};
+  // 执行原型链接
+  target.__proto__ = func.prototype;
+  // 执行函数，绑定this,
+  const result = func.call(target, ...arg);
+  // 判断返回值类型
+  if (result && (typeof result === 'object' || typeof result === 'function')) {
+    return result;
+  }
+  return target
 }
 
