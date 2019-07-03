b4b8a3b57b742dfca05856a5e02901f7cafd8b30
Kyrie Ving <834854385@qq.com>
2019-07-03 19:08:08 +0800

    增加字符次数统计

---
+++ String/Practice/characterCount.js
@@ -0,0 +1,44 @@
+/**
+ * 字符出现的次数
+ */
+
+
+function getCount(str) {
+  const arr = [...new Set(str.split(''))];
+  let obj = {}
+  arr.forEach(k => {
+    obj[k] = str.split(k).length - 1;
+  })
+  return obj
+}
+
+function getCount2(str) {
+  const obj = Object.create(null);
+
+  for (var i = 0; i < str.length - 1; i++) {
+    if (obj[str[i]]) {
+      obj[str[i]]++;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+
+  var num = 0;
+  var char = '';
+  for (var key in obj) {
+    if (obj[key] > num) {
+      num = obj[key];
+      char = key;
+    }
+  }
+
+  return { num, char }
+
+}
+
+
+// test
+
+var s = 'wewerereeeeeee2423432sdfdsff';
+getCount(s)
+getCount2(s)
