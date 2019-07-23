bde8addb818b1f0f6261012f099d2c3d20cdad13
Kyrie Ving <834854385@qq.com>
2019-07-23 10:09:59 +0800

    更新

--- Array/Normal/quickSort.js
+++ Array/Normal/quickSort.js
@@ -32,3 +32,41 @@ function quickSort(arr) {
 var arr = [23, 56, 77, 4, 45, 24, 29, 54];
 
 quickSort(arr)
+
+
+
+
+// 问题: 获取第 n 层的节点
+// 扩展: 顺序获取第 0 - n 层的节点，并依次装入数组
+// var s = {
+//   a1: {
+//     b1: { c1: 1 },
+//     b2: { c2: 1 }
+//   }
+// };
+// 则第 0 - 2 层的节点依次为: a1
+// b1、b2
+// c1、c2
+// 要求返回结果为
+// [['a1'], ['b1', 'b2'], ['c1', 'c2']]
+
+function isObject(param) {
+  return Object.prorotype.toString.call(param) === '[object Object]';
+}
+
+function getNode(data) {
+  if (!isObject(data)) {
+    return data;
+  }
+  let stashArr = [].concat(data);
+
+  stashArr.reduce((pre, cur) => {
+    const nextData = Object.keys(cur).map(k => cur[k]);
+    if (isObject())
+
+
+      return pre.push(Object.keys(cur))
+  }, [])
+
+
+}
