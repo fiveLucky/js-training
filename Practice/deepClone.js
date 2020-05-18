1c1ae123f635db37c54b8ff41bd5478d654e3604
Kyrie Ving <834854385@qq.com>
2020-05-18 17:11:01 +0800

    feat：update test case

--- Practice/deepClone.js
+++ Practice/deepClone.js
@@ -1,25 +1,23 @@
-
 /**
  * 实现一个对象深拷贝方法
  */
 
-
 function isObject(target) {
-  return target !== null && typeof target === 'object';
+  return target !== null && typeof target === "object";
 }
 
 function deepClone(data) {
   if (!isObject(data)) {
     return data;
   }
+
   // function 不用复制，只考虑对象和数组即可
   const target = Array.isArray(data) ? [] : {};
   // for in 遍历
   for (let k in data) {
     // 判断是否是实例属性
     if (data.hasOwnProperty(k)) {
-      const value = data[k];
-      target[k] = isObject(data) ? deepClone(value) : value;
+      target[k] = deepClone(data[k]);
     }
   }
 
@@ -29,18 +27,23 @@ function deepClone(data) {
 // test
 
 var obj = {
-  a: 'a',
-  b: [
-    {
-      c: 'c',
-      d: ['e', 'f']
-    },
-    [1, 2, 3, 4, 5]
-  ],
-  g: {
-    h: 6
-  }
-}
+  name: "obj",
+  get: function () {
+    return this, name;
+  },
+  count: undefined,
+  number: null,
+  face: Symbol("unique"),
+  hobbies: [1, 2, 3, 4],
+  life: {
+    eat: true,
+    sleep: true,
+  },
+};
+var result = deepClone(obj);
 
-console.log(deepClone(obj));
+result.name = "result";
+result.life.eat = false;
 
+console.log(obj);
+console.log(result);
